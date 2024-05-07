1912afaa87b1c963c26c035e18b2a1b254115eb4
JJCAPPE <giacomo.cappelletto@icloud.com>
2024-05-07 12:20:00 +0200

    django app

---
+++ speechapp/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#url configuration
+# path( 'url' , function )  --  no need to add mainapp/ as we configured all mainapp/ to be handled by this file in speechapp.urls.py
+
+urlpatterns = [
+    path('test/', views.test_request) 
+]
---
+++ speechapp/mainapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+#request handler (request --> response)
+
+#function is called in mainapp.urls.py to senf a response to the client
+
+def test_request(request):
+    return render(request, 'entrance.html', {'IT': 'Spanish'})
+
